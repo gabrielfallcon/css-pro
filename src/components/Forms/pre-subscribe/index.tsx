5c943bc30e9427b1186ccefd8b1536cc57b4629d
Gabriel Nascimento <gabriel.nascimento@ibm.com>
2023-01-09 12:07:53 -0300

    create boxImage

--- src/components/Forms/pre-subscribe/index.tsx
+++ src/components/Forms/pre-subscribe/index.tsx
@@ -1,14 +1,14 @@
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import api from '../../../services/api';
-import { Button } from '../../Button';
-import Input from '../../Input';
-import styles from './styles.module.scss';
-import {useRouter} from 'next/router';
-import { useState } from 'react';
-import Loading from '../../Loading';
-import { AxiosError } from 'axios';
+import api from "../../../services/api";
+import { Button } from "../../Button";
+import Input from "../../Input";
+import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Loading from "../../Loading";
+import clsx from "clsx";
 
 interface InputForm {
   name: string;
@@ -21,42 +21,45 @@ interface AxiosErrorMessage {
 }
 
 const schema = yup.object({
-  name: yup.string().required('É necessário o nome'),
-  email: yup.string().required('É necessario o email'),
-  phone: yup.string()
-})
-
+  name: yup.string().required("É necessário o nome"),
+  email: yup.string().required("É necessario o email"),
+  phone: yup.string(),
+});
 
 const PreSubscribeForm = () => {
   const [load, setLoad] = useState(false);
   const [existsUser, setExistsUser] = useState(false);
-  const {push} = useRouter();
+  const { push } = useRouter();
 
-  const { register, handleSubmit, setValue, formState: { errors } } = useForm<InputForm>({
-    resolver: yupResolver(schema)
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<InputForm>({
+    resolver: yupResolver(schema),
   });
 
   const clearInputs = () => {
-    setValue('name', '');
-    setValue('email', '');
-    setValue('phone', '');
-  }
+    setValue("name", "");
+    setValue("email", "");
+    setValue("phone", "");
+  };
 
   const onSubmit = async (data: any) => {
     setLoad(true);
 
     try {
-      const response = await api.post('subscribe', data);
-      clearInputs()
-      console.log(response, 'response');
-      push('/css-pro/success');
-      
+      const response = await api.post("subscribe", data);
+      clearInputs();
+      console.log(response, "response");
+      push("/css-pro/success");
     } catch (err) {
-      clearInputs()
+      clearInputs();
       setExistsUser(true);
     }
     setLoad(false);
-  }
+  };
 
   return (
     <div className={styles.boxForm}>
@@ -67,31 +70,33 @@ const PreSubscribeForm = () => {
       {!existsUser ? (
         <>
           <p className={styles.descriptionForm}>
-            Inscreva-se para ter vantagens exclusivas e a oportunidade de garantir sua vaga com antecedência.
+            Inscreva-se para ter vantagens exclusivas e a oportunidade de
+            garantir sua vaga com antecedência.
           </p>
           <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-            <Input 
-              label="Nome" 
-              {...register("name")} 
-              isRequired 
-              placeholder='Digite aqui seu primeiro nome' 
+            <Input
+              label="Nome"
+              {...register("name")}
+              isRequired
+              placeholder="Digite aqui seu primeiro nome"
               error={errors.name?.message}
             />
-            <Input 
-              label="E-mail" 
-              {...register("email")} 
-              isRequired 
-              placeholder='Digite aqui seu melhor e-mail' 
+            <Input
+              label="E-mail"
+              {...register("email")}
+              isRequired
+              placeholder="Digite aqui seu melhor e-mail"
               error={errors.email?.message}
             />
 
-            <Input label="Telefone" {...register("phone")} placeholder='Digite aqui seu telefone' />
+            <Input
+              label="Telefone"
+              {...register("phone")}
+              placeholder="Digite aqui seu telefone"
+            />
             <p>{errors.phone?.message}</p>
 
-
-            <Button type="submit">
-              Concluir Pré Inscrição
-            </Button>
+            <Button type="submit">Concluir Pré Inscrição</Button>
           </form>
         </>
       ) : (
@@ -99,23 +104,34 @@ const PreSubscribeForm = () => {
           <p className={styles.descriptionForm}>
             Este e-mail já está sendo utilizado em outro cadastro.
           </p>
-            <Button type="submit" onClick={() => {
-              clearInputs()
-              setExistsUser(false)
-            }}>
+          <Button
+            type="submit"
+            onClick={() => {
+              clearInputs();
+              setExistsUser(false);
+            }}
+          >
             Tentar com outro E-mail
           </Button>
         </>
       )}
 
-      <img 
-        src="/images/css-pro.svg" 
-        alt="CSS Pro" 
-        className={styles.mobileImage}
-      />
-      {load && <Loading/>}
+      <div className={styles.boxImg}>
+        <img
+          src="/images/css-pro-new.svg"
+          alt="CSS Pro"
+          className={styles.imageArt}
+        />
+
+        <div className={clsx(styles.blurImage, styles.bottomLeftImage)} />
+
+        <div className={clsx(styles.blurImage, styles.bottomRightImage)} />
+
+        <div className={clsx(styles.blurImage, styles.topRightImage)} />
+      </div>
+      {load && <Loading />}
     </div>
-  )
-}
+  );
+};
 
 export default PreSubscribeForm;
--- src/pages/css-pro/pre-inscricao/index.tsx
+++ src/pages/css-pro/pre-inscricao/index.tsx
@@ -1,52 +1,36 @@
 import { Counter } from "../../../components/Counter";
-import clsx from 'clsx';
+import clsx from "clsx";
 
-import styles from './styles.module.scss';
-import PreSubscribeForm from '../../../components/Forms/pre-subscribe';
+import styles from "./styles.module.scss";
+import PreSubscribeForm from "../../../components/Forms/pre-subscribe";
 
 const PreInscricao = () => {
   return (
     <>
-      <section className={styles.section}> 
+      <section className={styles.section}>
         <PreSubscribeForm />
 
-        <img 
-          src="/images/css-pro.svg" 
-          alt="CSS Pro" 
-          className={styles.imageArt}
-        />
+        <div className={styles.boxImg}>
+          <img
+            src="/images/css-pro-new.svg"
+            alt="CSS Pro"
+            className={styles.imageArt}
+          />
+
+          <div className={clsx(styles.blurImage, styles.bottomLeftImage)} />
+
+          <div className={clsx(styles.blurImage, styles.bottomRightImage)} />
+
+          <div className={clsx(styles.blurImage, styles.topRightImage)} />
+        </div>
       </section>
 
-      <div 
-        className={clsx(
-          styles.blur,
-          styles.blue,
-          styles.topLeft
-        )}
-      />
-      <div 
-        className={clsx(
-          styles.blur,
-          styles.blue,
-          styles.topRight
-        )}
-      />
-      <div 
-        className={clsx(
-          styles.blur,
-          styles.green,
-          styles.bottomLeft
-        )}
-      />
-      <div 
-        className={clsx(
-          styles.blur,
-          styles.green,
-          styles.bottomRight
-        )}
-      />
+      <div className={clsx(styles.blur, styles.blue, styles.topLeft)} />
+      <div className={clsx(styles.blur, styles.blue, styles.topRight)} />
+      <div className={clsx(styles.blur, styles.green, styles.bottomLeft)} />
+      <div className={clsx(styles.blur, styles.green, styles.bottomRight)} />
     </>
-  )
-}
+  );
+};
 
 export default PreInscricao;
